700ed250634125bb318503d8322ee062a5e16791
ahamouda <a.hammoda@nu.edu.eg>
2023-10-17 16:06:51 +0300

    antmatchers

--- userMicroservice/src/main/java/com/backproject/userMicroservice/config/SecurityConfig.java
+++ userMicroservice/src/main/java/com/backproject/userMicroservice/config/SecurityConfig.java
@@ -1,18 +1,14 @@
 package com.backproject.userMicroservice.config;
 
-import com.backproject.userMicroservice.models.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import static com.backproject.userMicroservice.models.Role.USER;
 
 @Configuration
 @EnableWebSecurity
@@ -24,13 +20,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(req->req
-                                .requestMatchers("/users/**")
-                                .permitAll())
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf().disable()
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers("/users/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
 
         return http.build();
     }
